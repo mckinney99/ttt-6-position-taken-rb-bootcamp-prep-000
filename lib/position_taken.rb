cee69b9b9776db4ba1f27cd8038e8de844c024b2
Eric McKinney <mckinney99@gmail.com>
2017-10-11 03:37:40 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-    if board[index] == " "
+    if board[index] == nil
       false
-    elsif board[index] == nil
+    elsif board[index] == " "
         false
 
 end
